099c0223e072bb0cf5aff5a022dba385f7e34089
Manuel Möhlmann <mail@manmoe.com>
2014-03-07 15:58:37 +0100

    added popup logic

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -16,6 +16,45 @@ requirejs(
 
     function (jquery)
     {
-        // code here
+        window.onload = function(){
+            jquery.ajax({type:'GET', dataType:'xml', url: "http://www.newsslash.com/rss", timeout:5000, success:parseFeed, error:handleError, async: false});
+        }
+
+        function parseFeed(feed) {
+            console.log(document);
+
+            var feedTable = document.getElementById("feed");
+            var count = 0;
+
+            jquery('item', feed).each( function() {
+                var row = document.createElement("tr");
+                row.className = "link";
+                var number = document.createElement("td");
+                number.innerText = ++count;
+                var linkContainer = document.createElement("td");
+
+                var link = document.createElement("a");
+                link.innerText = jquery(this).find('title').eq(0).text();
+                link.href = jquery(this).find('link').eq(0).text();
+                link.addEventListener("click", openTab);
+
+                linkContainer.appendChild(link)
+
+                row.appendChild(number);
+                row.appendChild(linkContainer);
+
+                feedTable.appendChild(row);
+            });
+
+
+        }
+
+        function openTab() {
+            chrome.tabs.create({url: this.href, selected: false});
+        }
+
+        function handleError() {
+            console.log("Error while parsing.")
+        }
     }
 );
