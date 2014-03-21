4238455d908a58265f4b540eb6f2b0a072522983
Manuel Möhlmann <mail@manmoe.com>
2014-03-21 16:43:28 +0100

    added script for making *.crx files

---
+++ crxmake.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+#
+# Purpose: Pack a Chromium extension directory into crx format
+
+if test $# -ne 1; then
+  echo "Usage: crxmake.sh <extension dir>"
+  echo "Make sure, that you have exported the private key"
+  exit 1
+fi
+
+dir=$1
+key="key.pem"
+
+# add header line of private key
+echo "-----BEGIN PRIVATE KEY-----" > key.pem
+
+# get private key from env and replace all whitespaces with linebreaks
+echo $PEMKEY | tr '|' '\n' >> key.pem
+
+# add base line of private key
+echo "-----END PRIVATE KEY-----" >> key.pem
+
+name="newsslash"
+crx="$name.crx"
+pub="$name.pub"
+sig="$name.sig"
+sig="$name.sig"
+zip="$name.zip"
+trap 'rm -f "$pub" "$sig" "$zip"' EXIT
+
+# zip up the crx dir
+cwd=$(pwd -P)
+(cd "$dir" && zip -qr -9 -X "$cwd/$zip" .)
+
+# signature
+openssl sha1 -sha1 -binary -sign "$key" < "$zip" > "$sig"
+
+# public key
+openssl rsa -pubout -outform DER < "$key" > "$pub" 2>/dev/null
+
+byte_swap () {
+  # Take "abcdefgh" and return it as "ghefcdab"
+  echo "${1:6:2}${1:4:2}${1:2:2}${1:0:2}"
+}
+
+crmagic_hex="4372 3234" # Cr24
+version_hex="0200 0000" # 2
+pub_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$pub" | awk '{print $5}')))
+sig_len_hex=$(byte_swap $(printf '%08x\n' $(ls -l "$sig" | awk '{print $5}')))
+(
+  echo "$crmagic_hex $version_hex $pub_len_hex $sig_len_hex" | xxd -r -p
+  cat "$pub" "$sig" "$zip"
+) > "$crx"
+echo "Wrote $crx"
+rm $key
